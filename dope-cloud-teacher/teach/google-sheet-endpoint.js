5ee364dfea2ca4b7ef149c93b01af9f0966f5af4
RoSeé Murphy <51420322+MooreTechnologyStrategists@users.noreply.github.com>
2026-09-22 18:28:07 -0400

    Configure instructor application Google endpoint

--- dope-cloud-teacher/teach/google-sheet-endpoint.js
+++ dope-cloud-teacher/teach/google-sheet-endpoint.js
@@ -1,2 +1,1 @@
-// Paste the deployed Google Apps Script Web app URL here after its one-time setup.
-window.DCT_INSTRUCTOR_ENDPOINT = '';
+window.DCT_INSTRUCTOR_ENDPOINT = 'https://script.google.com/macros/s/AKfycby-mt1BSD6ABMlVe8uhVOjafSi6bJKbubpcv2vtIEXwaz_dRBRJtIi0ay6ZF6sBEysl/exec';
